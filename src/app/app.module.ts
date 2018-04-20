fb1db8f5d33c4b9f8c60a55d01d73d40d034a169
sathyapv92 <sathapv92@gmail.com>
2018-04-20 09:43:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+import {RouterModule} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Child1Component,
+    Child2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule ,
+    RouterModule.forRoot([
+      {
+        path:"home",
+        component:HomeComponent,
+        children: [
+        
+          {path: 'tracks', component: Child1Component},
+          {path: 'albums', component: Child2Component}
+        ]
+      },
+     
+     ],{ enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
